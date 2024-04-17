425649e2aae851cf3e0f5cd1225a74dd1f35e795
Sanilal P Sathyan <sanilal@gmail.com>
2024-04-17 15:33:38 +0400

    Home page structure

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,15 @@
 export default function Home() {
   return (
-    <div>Home</div>
+    <section className="w-full flex-center flex-col">
+        <h1 className="head_text"> Discover & Share AI Prompts
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center w-full block">AI-Powered Prompts</span>
+        </h1>
+        <p className="desc text-center">
+            It is an experiment project to create and share  prompts that can be used as starting points for writing or generating ideas. 
+        </p>
+        {/* Feed */}
+
+    </section>
   )
 }
