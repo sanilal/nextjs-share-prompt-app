ebbfbd8904a3d5540e23ccb260ae96f6299311b3
Sanilal P Sathyan <sanilal@gmail.com>
2024-04-18 18:01:47 +0400

    mongo db adding progress

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from 'next-auth/providers/google';
 
-
+import { connectToDB } from '@utils/database';
 
 const handler = NextAuth({
     providers: [
@@ -15,9 +15,15 @@ const handler = NextAuth({
     },
     async signIn({ profile }) {
         try {
+            await connectToDB();
+            //  check if a user already exists
 
+            // if not, create a new user
+
+            return true;
         } catch (error) {
-
+            console.log(error);
+            return false;
         }
     }
 })
