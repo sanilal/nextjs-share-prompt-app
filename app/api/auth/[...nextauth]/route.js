708f0338e8e63da9fd2e8450b79f4d712ead0917
Sanilal P Sathyan <sanilal@gmail.com>
2024-04-19 10:04:04 +0400

    route js

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from 'next-auth/providers/google';
 
+import User from "@models/user";
 import { connectToDB } from '@utils/database';
 
 const handler = NextAuth({
@@ -11,14 +12,25 @@ const handler = NextAuth({
         })
     ],
     async session({ session }) {
-
+        const sessionUser = await User.findOne({
+            email:session.user.email
+        })
+        session.user.id = sessionUser._id.toString();
     },
     async signIn({ profile }) {
         try {
             await connectToDB();
             //  check if a user already exists
+            const userExists = await User.findOne({ email: profile.email });
 
             // if not, create a new user
+            if(!userExists) {
+                await User.create({
+                    email: profile.email,
+                    username: profile.name.replace(" ", "").toLowerCase(),
+                    image: profile.picture
+                });
+            }
 
             return true;
         } catch (error) {
